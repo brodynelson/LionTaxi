338a4063bcaa304863bf4e162bc24960b7768156
Brody Nelson <brodynelson@gmail.com>
2012-06-21 09:53:23 +0100

    Initial Commit

---
+++ LionTaxi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LionTaxi
+//
+//  Created by Brody Nelson on 21/06/12.
+//  Copyright (c) 2012 Digital Projects Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LionTaxi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LionTaxi
+//
+//  Created by Brody Nelson on 21/06/12.
+//  Copyright (c) 2012 Digital Projects Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
